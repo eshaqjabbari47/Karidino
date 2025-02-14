0b266543b36ca40e023a4e8adbedf4558b6f6058
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-14 16:29:50 +0330

    create entity class

--- app/src/main/java/com/eshaq/kharidino/model/room/Item.kt
+++ app/src/main/java/com/eshaq/kharidino/model/room/Item.kt
@@ -1,4 +1,12 @@
 package com.eshaq.kharidino.model.room
 
-interface Item {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Item (
+    @PrimaryKey(autoGenerate = true)
+    val id:Int =0,
+    val name:String,
+    val isBought: Boolean=false
+)
