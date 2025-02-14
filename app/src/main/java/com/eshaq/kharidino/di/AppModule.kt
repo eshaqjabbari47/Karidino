36f863af573e91fd5774129b6fa7cbb35850df09
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-14 16:59:00 +0330

    combine room with hilt

--- app/src/main/java/com/eshaq/kharidino/di/AppModule.kt
+++ app/src/main/java/com/eshaq/kharidino/di/AppModule.kt
@@ -1,4 +1,30 @@
 package com.eshaq.kharidino.di
 
+import android.content.Context
+import androidx.room.Room
+import com.eshaq.kharidino.model.repository.ItemRepository
+import com.eshaq.kharidino.model.room.AppDataBase
+import com.eshaq.kharidino.model.room.ItemDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context):AppDataBase = Room.databaseBuilder(context,AppDataBase::class.java,"kharidino_db")
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    fun provideItemDao(appDataBase: AppDataBase):ItemDao=appDataBase.ItemDao()
+
+    @Provides
+    fun provideItemRepository(appDataBase: AppDataBase):ItemRepository=ItemRepository(ItemDao)
 }
