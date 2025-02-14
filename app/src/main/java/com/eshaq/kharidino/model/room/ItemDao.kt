a11faabeae753f0f0f843e0404d3f7024c02f03c
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-14 16:44:52 +0330

    create itemdao

--- app/src/main/java/com/eshaq/kharidino/model/room/ItemDao.kt
+++ app/src/main/java/com/eshaq/kharidino/model/room/ItemDao.kt
@@ -1,4 +1,33 @@
 package com.eshaq.kharidino.model.room
 
-class ItemDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+
+    @Query("SELECT * From item")
+    fun getAllItems(): Flow<List<Item>>
+
+    @Insert
+    suspend fun insertItem(item: Item)
+
+    @Update
+    suspend fun updateItem(item: Item)
+
+    @Update
+    suspend fun buyFlagItem(item: Item)
+
+    @Update
+    suspend fun buyAllItems ()
+
+    @Delete
+    suspend fun deleteItem(item: Item)
+
+    @Delete
+    suspend fun deleteAllItems()
 }
